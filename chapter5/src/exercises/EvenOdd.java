f82d0208c7fb41dfcd78abcc3780af3190e73109
SuperSaiyan99-SS99 <SuperSaiyan99-SS99@users.noreply.github.com>
2017-09-28 08:13:17 -0500

    loops

--- chapter5/src/exercises/EvenOdd.java
+++ chapter5/src/exercises/EvenOdd.java
@@ -6,15 +6,22 @@ public class EvenOdd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int response = 1;
 		int number;
 		Scanner input = new Scanner(System.in);
-		
-		System.out.print("Enter an integer. >>> ");
-		number = input.nextInt();
-		if (number % 2 == 0)
-			System.out.println("The number is even.");
-		else
-			System.out.println("The number is odd.");
+		while (response == 1) {
+			System.out.print("Enter an integer. >>> ");
+			number = input.nextInt();
+			if (number % 2 == 0) {
+				System.out.println("The number is even.");
+			}
+			else {
+				System.out.println("The number is odd.");
+			}
+			System.out.println("Would you like to enter another int? 1 for yes, 2 for no. >>> ");
+			response = input.nextInt();
+		}
+		System.out.println("You have exited the game.");
 	}
 
 }
--- chapter5/src/gameZone/RockPaperScissors.java
+++ chapter5/src/gameZone/RockPaperScissors.java
@@ -10,66 +10,74 @@ public class RockPaperScissors {
 		String randomNumberString;
 		String result;
 		int number;
+		int score = 0;
 		int randomNumber = (1 + (int)(Math.random() * 3));
 		
-		numberString = JOptionPane.showInputDialog(null, "Please choose:\n1 - Rock\n2 - Paper\n3 - Scissors");
-		number = Integer.parseInt(numberString);
-		if (number ==  1 && number == randomNumber) {
-			numberString = "rock";
-			randomNumberString = "rock";
-			result = "the result was a tie.";
-			JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
-		}
-		else if (number ==  2 && number == randomNumber) {
-			numberString = "paper";
-			randomNumberString = "paper";
-			result = "the result was a tie.";
-			JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
-		}
-		else if (number ==  3 && number == randomNumber) {
-			numberString = "scissors";
-			randomNumberString = "scissors";
-			result = "the result was a tie.";
-			JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
-		}
-		else if (number ==  1 && randomNumber == 2) {
-			numberString = "rock";
-			randomNumberString = "paper";
-			result = "I win.";
-			JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
-		}
-		else if (number ==  1 && randomNumber == 3) {
-			numberString = "rock";
-			randomNumberString = "scissors";
-			result = "you win.";
-			JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
-		}
-		else if (number ==  2 && randomNumber == 1) {
-			numberString = "paper";
-			randomNumberString = "rock";
-			result = "you win.";
-			JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
-		}
-		else if (number ==  2 && randomNumber == 3) {
-			numberString = "paper";
-			randomNumberString = "scissors";
-			result = "I win.";
-			JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
-		}
-		else if (number ==  3 && randomNumber == 1) {
-			numberString = "scissors";
-			randomNumberString = "rock";
-			result = "I win.";
-			JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
-		}
-		else if (number ==  3 && randomNumber == 2) {
-			numberString = "scissors";
-			randomNumberString = "paper";
-			result = "you win.";
-			JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
-		}
-		else {
-			JOptionPane.showMessageDialog(null, "You're a fool for choosing such a losing number.");
+		for (int x = 0; x < 5; x ++) {
+		
+			numberString = JOptionPane.showInputDialog(null, "Please choose:\n1 - Rock\n2 - Paper\n3 - Scissors");
+			number = Integer.parseInt(numberString);
+			if (number ==  1 && number == randomNumber) {
+				numberString = "rock";
+				randomNumberString = "rock";
+				result = "the result was a tie.";
+				JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
+			}
+			else if (number ==  2 && number == randomNumber) {
+				numberString = "paper";
+				randomNumberString = "paper";
+				result = "the result was a tie.";
+				JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
+			}
+			else if (number ==  3 && number == randomNumber) {
+				numberString = "scissors";
+				randomNumberString = "scissors";
+				result = "the result was a tie.";
+				JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
+			}
+			else if (number ==  1 && randomNumber == 2) {
+				numberString = "rock";
+				randomNumberString = "paper";
+				result = "I win.";
+				JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
+			}
+			else if (number ==  1 && randomNumber == 3) {
+				numberString = "rock";
+				randomNumberString = "scissors";
+				result = "you win.";
+				score ++;
+				JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
+			}
+			else if (number ==  2 && randomNumber == 1) {
+				numberString = "paper";
+				randomNumberString = "rock";
+				result = "you win.";
+				score ++;
+				JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
+			}
+			else if (number ==  2 && randomNumber == 3) {
+				numberString = "paper";
+				randomNumberString = "scissors";
+				result = "I win.";
+				JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
+			}
+			else if (number ==  3 && randomNumber == 1) {
+				numberString = "scissors";
+				randomNumberString = "rock";
+				result = "I win.";
+				JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
+			}
+			else if (number ==  3 && randomNumber == 2) {
+				numberString = "scissors";
+				randomNumberString = "paper";
+				result = "you win.";
+				score ++;
+				JOptionPane.showMessageDialog(null, "You chose " + numberString + " and I chose " + randomNumberString + " so " + result);
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "You're a fool for choosing such a losing number.");
+			}
+			JOptionPane.showMessageDialog(null, "Your score is " + score);
 		}
 	}
 
